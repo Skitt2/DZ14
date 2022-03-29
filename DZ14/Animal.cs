77eb00964193e9a07a6939a2f058b44544c54170
Skitt2 <provaid@yandex.ru>
2022-03-30 00:44:01 +0300

    Добавьте файлы проекта.

---
+++ DZ14/Animal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Cat(string name)
+            : base(name)
+        {
+            this.name = name;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+    class Dog : Animal
+    {
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name)
+            : base(name)
+        {
+            this.name = name;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
---
+++ DZ14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat("Кошка");
+            cat.ShowInfo();
+            Animal dog = new Dog("Собака");
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
